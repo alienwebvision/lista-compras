b33ef90372d33e21184de48e72c593fe5e0b0b57
alienware <alienwebvisin@gmail.com>
2023-10-24 13:41:15 -0300

    Alteração das listas para listas de compras e seus proprietários

--- app-lista-compras/resources/views/components/delete.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-
-    <form action="{{ route('supports.destroy', $support->id) }}" method="post">
-        @csrf()
-        @method('DELETE')
-        <button class="btn btn-danger" type="submit">Deletar</button>
-    </form>
